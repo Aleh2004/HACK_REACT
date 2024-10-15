4002e9e6ea30d81af1afca12841e06fce52005da
Aleh2004 <alchaer2004@gmail.com>
2024-10-15 01:51:55 -0400

    subiendo hack react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserList from './UserList';
+import UserForm from './UserForm';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUser = (user) => {
+    setUsers([...users, user]);
+  };
+
+  const deleteUser = (email) => {
+    setUsers(users.filter(user => user.email !== email));
+  };
+
+  const updateUser = (updatedUser) => {
+    setUsers(users.map(user => user.email === updatedUser.email ? updatedUser : user));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Gestión de Usuarios</h1>
+      <UserForm addUser={addUser} />
+      <UserList users={users} deleteUser={deleteUser} updateUser={updateUser} />
+      <p>Total de usuarios: {users.length}</p>
     </div>
   );
 }
